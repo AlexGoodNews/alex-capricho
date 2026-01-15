7628f29835d38b678512d1172fa40d6d8c8a0239
AlexGoodNews <a.cavero@goodnews.es>
2026-01-15 11:12:16 +0100

    getPuntos3

--- functions/get-puntos.js
+++ functions/get-puntos.js
@@ -1,12 +1,6 @@
 export async function onRequest() {
-  const hasKV = typeof CMS_ALEXPUNTOS !== 'undefined'
-  const value = hasKV ? await CMS_ALEXPUNTOS.get('puntos') : null
-
-  return new Response(
-    JSON.stringify({
-      hasKV,
-      value
-    }),
-    { headers: { 'Content-Type': 'application/json' } }
-  )
+  const data = await CMS_PUNTOS.get('puntos')
+  return new Response(data, {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
